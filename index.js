b62f309f34e635cafa931c7b04f584313ea198b3
gblatstein@gmail.com <gblatstein@gmail.com>
2018-08-16 17:41:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function theBeatlesPlay([musicians], [instrument]){
   var array[]
-  for var i=0; i = musicians.length`${musicians} plays ${instrument}`
+  for var i=0; i < musicians.length;`${musicians} plays ${instrument}`
 }
   
