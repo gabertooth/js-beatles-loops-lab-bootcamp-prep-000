12dc59fd18a41cf9bbe4fd4def26b6df81b3d724
gblatstein@gmail.com <gblatstein@gmail.com>
2018-08-16 17:45:33 +0000

    automatically backed up by learn

--- index.js
+++
@@ -1,7 +0,0 @@
-function theBeatlesPlay(musicians, instruments){
-  var array=[];
-  for (var i=0; i < musicians.length;i++){
-  array.push(musicians[i] + 'plays'+ instrument[i]);
-}
-return array;
-}
